7c20a938cee504e20043711425418c3b7d35cb5a
mcstr <quecontentoestoy@gmail.com>
2022-06-22 14:20:13 +0200

    snake class

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
+class Socket {
+
+    constructor (url) {
+        this.socket = new WebSocket('ws://10.62.143.9:8080/test');
+    }    
+
+    init() {
+        this.socket.addEventListener('open', (e) =>{
+            this.socket.send('Hello Server');
+        }) 
+        this.socket.addEventListener('message', function (e){
+    this.id = e.data.split("/")[0];
+        })
+    }
+
+    updatePosition(x, y) {
+        console.log(x + ', ' + y + '\n');
+        this.socket.send(x + '/' + y);
+    }
+}
+
+
+
+
+
 const canvas = document.getElementById('game');
 const context = canvas.getContext("2d");
 window.addEventListener('keydown', keyEvents);  
@@ -6,7 +31,8 @@ let positionY = Math.random() *  ((canvas.height - 100) - 100) + 100;
 let direction  = 0;
 let distance = 0.5; //steplength
 let turnSpeed = 2;
-
+const socket = new Socket('adklfjaöd');
+let id = NaN;
 
 async function init() {
     context.beginPath();
@@ -14,7 +40,6 @@ async function init() {
     context.lineWidth = "5";
     context.moveTo(positionX, positionY);
     direction = Math.random() *360;
-    socket = new Socket('adklfjaöd');
 
     while (true) {
             await new Promise(r => setTimeout(r, 16));
@@ -46,27 +71,6 @@ function calculateNextPosition(positionX, positionY, direction, distance) {
     let x1 = positionX + distance * Math.cos(direction * Math.PI / 180);
     let y1 = positionY + distance * Math.sin(direction * Math.PI / 180);
     return { x1, y1};
-}
-
-class Socket {
-
-    constructor (url) {
-        this.socket = new WebSocket('ws://10.62.142.87:8080/event-emitter');
-    }    
-
-    init() {
-        this.socket.addEventListener('open', (e) =>{
-            this.socket.send('Hello Server');
-        }) 
-        this.socket.addEventListener('message', function (e){
-            console.log(e.data);
-        })
-    }
-
-    updatePosition(x, y) {
-        console.log(x + ', ' + y + '\n');
-        this.socket.send(x + '\n' + y);
-    }
 }
 
 init();
