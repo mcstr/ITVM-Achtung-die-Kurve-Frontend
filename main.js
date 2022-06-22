610171db9336fff6a527495747026dfbf42a648a
vondrehleyannik <yanvondre@t-online.de>
2022-06-22 10:56:59 +0200

    Sockets und Kurven

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const canvas = document.getElementById('game');
 const context = canvas.getContext("2d");
 window.addEventListener('keydown', keyEvents);  
-let positionX = 500;
-let positionY = 500;
+let positionX = Math.random() * ((canvas.width - 100) - 100) + 100;
+let positionY = Math.random() *  ((canvas.height - 100) - 100) + 100;
 let direction  = 0;
 let distance = 0.5; //steplength
 let turnSpeed = 2;
@@ -14,25 +14,29 @@ async function init() {
     context.lineWidth = "5";
     context.moveTo(positionX, positionY);
     direction = Math.random() *360;
+    socket = new Socket('adklfjaöd');
 
     while (true) {
             await new Promise(r => setTimeout(r, 16));
             positionX = calculateNextPosition(positionX, positionY, direction, distance).x1;
             positionY = calculateNextPosition(positionX, positionY, direction, distance).y1;
+
+            socket.updatePosition(positionX, positionY);
+
             context.lineTo(positionX, positionY);
             context.stroke(); // Draw it
             context.moveTo(positionX, positionY); // Start from the new position
-            console.log(direction);
+            //console.log(direction);
     }
 }
 
 function keyEvents(e) {
     // links
-    if(e.keyCode === 37) {
+    if(e.keyCode === 37 || e.keyCode === 65) {
         direction -= turnSpeed;        
     }
     // rechst
-    if(e.keyCode === 39) {
+    if(e.keyCode === 39 || e.keyCode === 68) {
         direction += turnSpeed;
     }
 }
@@ -48,7 +52,6 @@ class Socket {
 
     constructor (url) {
         this.socket = new WebSocket('ws://10.62.142.87:8080/event-emitter');
-
     }    
 
     init() {
@@ -61,6 +64,9 @@ class Socket {
     }
 
     updatePosition(x, y) {
+        console.log(x + ', ' + y + '\n');
         this.socket.send(x + '\n' + y);
     }
 }
+
+init();
